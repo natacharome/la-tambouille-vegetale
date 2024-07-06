e9ad09c7d15110cdae26fb50187ac162b7fabf60
Natacha Rome <natacha-rome@hotmail.fr>
2024-07-06 23:37:27 +0200

    change a for div

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -11,9 +11,9 @@ const Card: React.FC<CardProps> = ({ id, data }) => {
   const imageUrl = data?.Images ? data?.Images[0].url : false;
   return (
     <>
-      <a className="cursor-pointer rounded-md m-4" onClick={handleClick}>
+      <div className="cursor-pointer rounded-md m-4" onClick={handleClick}>
         <img src={imageUrl} alt="Card" className="max-w-xs transition duration-500 ease-in-out hover:scale-105 object-cover w-80 h-80 rounded-md" />
-      </a>
+      </div>
     </>
   );
 };
