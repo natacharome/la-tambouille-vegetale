39a88f7621029f6161a47b0386d51363e908ca40
Natacha Rome <natacha-rome@hotmail.fr>
2024-07-24 16:23:56 +0200

    feat: add framer motion and a loader indicator

--- src/pages/Recipe/index.tsx
+++ src/pages/Recipe/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import RichTextComponent from "../../components/RichText";
 import { useGetRecipeById } from "../../hooks/recipes/useGetRecipe";
+import { motion, useScroll } from "framer-motion";
 
 const RecipePage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { data, isLoading } = useGetRecipeById(id || "");
+  const { scrollYProgress } = useScroll();
 
   return isLoading ? (
     <div role="status" className="flex justify-center items-center mt-20">
@@ -28,51 +30,57 @@ const RecipePage: React.FC = () => {
       <span className="sr-only">Loading...</span>
     </div>
   ) : (
-    <div className="flex flex-col lg:flex-row md:justify-between mb-20">
-      <div className="flex flex-col md:mr-4 overflow-y-auto">
+    <>
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-[5px] bg-primary origin-left"
+        style={{ scaleX: scrollYProgress }}
+      />
+      <div className="flex flex-col lg:flex-row md:justify-between mb-20">
+        <div className="flex flex-col md:mr-4 overflow-y-auto">
+          <div>
+            <button onClick={() => window.history.back()}>
+              <svg
+                width="79"
+                height="79"
+                viewBox="0 0 79 79"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M66.3606 39.4999C66.3606 54.3519 54.3526 66.3599 39.5006 66.3599C24.6486 66.3599 12.6406 54.3519 12.6406 39.4999C12.6406 24.6479 24.6486 12.6399 39.5006 12.6399C54.3526 12.6399 66.3606 24.6479 66.3606 39.4999ZM15.8006 39.4999C15.8006 52.6139 26.3866 63.1999 39.5006 63.1999C52.6146 63.1999 63.2006 52.6139 63.2006 39.4999C63.2006 26.3859 52.6146 15.7999 39.5006 15.7999C26.3866 15.7999 15.8006 26.3859 15.8006 39.4999Z"
+                  fill="#C8C8C8"
+                />
+                <path
+                  d="M42.1845 26.3857L29.0705 39.4997L42.1845 52.6137L39.9725 54.8257L24.6465 39.4997L39.9725 24.1737L42.1845 26.3857Z"
+                  fill="#C8C8C8"
+                />
+                <path
+                  d="M26.8594 41.0801L26.8594 37.9201L53.7194 37.9201L53.7194 41.0801L26.8594 41.0801Z"
+                  fill="#C8C8C8"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="mr-0 md:mr-20">
+            <h2 className="text-5xl md:text-6xl lg:text-7xl font-semibold">
+              {data?.fields?.Name.toUpperCase()}
+            </h2>
+            <h3 className="mt-16 font-bold text-neutral-900 mb-5">RECETTE: </h3>
+            <RichTextComponent richText={data?.fields?.Description} />
+          </div>
+        </div>
         <div>
-          <button onClick={() => window.history.back()}>
-            <svg
-              width="79"
-              height="79"
-              viewBox="0 0 79 79"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M66.3606 39.4999C66.3606 54.3519 54.3526 66.3599 39.5006 66.3599C24.6486 66.3599 12.6406 54.3519 12.6406 39.4999C12.6406 24.6479 24.6486 12.6399 39.5006 12.6399C54.3526 12.6399 66.3606 24.6479 66.3606 39.4999ZM15.8006 39.4999C15.8006 52.6139 26.3866 63.1999 39.5006 63.1999C52.6146 63.1999 63.2006 52.6139 63.2006 39.4999C63.2006 26.3859 52.6146 15.7999 39.5006 15.7999C26.3866 15.7999 15.8006 26.3859 15.8006 39.4999Z"
-                fill="#C8C8C8"
-              />
-              <path
-                d="M42.1845 26.3857L29.0705 39.4997L42.1845 52.6137L39.9725 54.8257L24.6465 39.4997L39.9725 24.1737L42.1845 26.3857Z"
-                fill="#C8C8C8"
-              />
-              <path
-                d="M26.8594 41.0801L26.8594 37.9201L53.7194 37.9201L53.7194 41.0801L26.8594 41.0801Z"
-                fill="#C8C8C8"
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="mr-0 md:mr-20">
-          <h2 className="text-5xl md:text-6xl lg:text-7xl font-semibold">
-            {data?.fields?.Name.toUpperCase()}
-          </h2>
-          <h3 className="mt-16 font-bold text-neutral-900 mb-5">RECETTE: </h3>
-          <RichTextComponent richText={data?.fields?.Description} />
+          {data?.fields?.Images.map((image: any, index: number) => (
+            <img
+              key={index}
+              className="rounded-3xl object-cover h-[90vh] w-[90%] mb-5"
+              alt="recipe-images"
+              src={image.url}
+            />
+          ))}
         </div>
       </div>
-      <div>
-        {data?.fields?.Images.map((image: any, index: number) => (
-          <img
-            key={index}
-            className="rounded-3xl object-cover h-[90vh] w-[90%] mb-5"
-            alt="recipe-images"
-            src={image.url}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
